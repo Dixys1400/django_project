4a371b1fb5588d95bcd0cc3069cb7161deac53b1
Dixys1400 <dimamaper@gmail.com>
2025-06-08 20:20:37 +0200

    Добавил регистрацию и логинизацию с JWT

--- clothing_store/boutique/urls.py
+++ clothing_store/boutique/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ClothingItemViewSet, home
+from .views import ClothingItemViewSet, home, RegisterAPIView
 from . import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
@@ -15,10 +15,10 @@ router.register(r'clothing', ClothingItemViewSet)
 urlpatterns = [
     path('catalog', views.catalog_view, name='catalog'),
     path('api/', include(router.urls)),
-    # path('register/', RegisterAPIView.as_view(), name='register'),
+    path('register/', RegisterAPIView.as_view(), name='register'),
     path('home/', home, name='home'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh')
 ]
 
 
--- clothing_store/boutique/views.py
+++ clothing_store/boutique/views.py
@@ -1,30 +1,43 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-#from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from rest_framework import viewsets, status
-#from rest_framework.response import Response
-#from rest_framework.views import APIView
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import ClothingItem
 from .serializers import ClothingItemSerializer
 
 
-# Регистрация через APIView, также в urls.py закоментировал путь
-# 🔒 СТАРАЯ ВЕРСИЯ РЕГИСТРАЦИИ (оставлено для примера)
 
-#class RegisterAPIView(APIView):
-#    def post(self, request):
-#        username = request.data.get('username')
-#        password = request.data.get('password')
-#        email = request.data.get('email')
-#
-#        if not username or password:
-#            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#        if User.objects.filter(username=username).exists():
-#            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#        user = User.objects.create_user(username=username, password=password, email=email)
-#        return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password:
+            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        user = User.objects.create(
+            username=username,
+            password=make_password(password),
+            email=email
+        )
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'message': 'User registered successfully',
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
 
 
 
@@ -33,7 +46,7 @@ from .serializers import ClothingItemSerializer
 
 class ClothingItemViewSet(viewsets.ModelViewSet):
     queryset = ClothingItem.objects.all()
-    serializers_class = ClothingItemSerializer
+    serializer_class = ClothingItemSerializer
 
 
 def catalog_view(request):
